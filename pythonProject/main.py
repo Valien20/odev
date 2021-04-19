5139dd523bcbae533e0efb1bd83281ea601fa658
Valien20 <72169138+Valien20@users.noreply.github.com>
2021-04-19 22:12:39 +0300

    Update main.py

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,16 +1,63 @@
-# This is a sample Python script.
+print(" Notlarınızı 0-100 arasında uygun şekilde doldurun.")
+kisasinav1=input("kisasinav1:")
+x=float(kisasinav1)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+if x<0 or x>100:
+    print("Lütfen not aralığında sayısal bir deger giriniz.")
+elif type(x) is not float:
+    print("lütfen sayısal bir değer girin.")
+kisasinav2=input("kisasinav2:")
+y=float(kisasinav2)
 
+if y<0 or y>100:
+    print("Lütfen not aralığında sayısal bir deger giriniz.")
+elif type(y) is not float:
+    print("lütfen sayısal bir değer girin.")
+arasinav=input("arasinav:")
+z=float(arasinav)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+if z<0 or z>100:
+    print("Lütfen not aralığında sayısal bir deger giriniz.")
+elif type(z) is not float:
+    print("lütfen sayısal bir değer girin.")
 
+odev1=input("odev1:")
+t=float(odev1)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+if t<0 or t>100:
+    print("Lütfen not aralığında sayısal bir deger giriniz.")
+elif type(t) is not float:
+    print("lütfen sayısal bir değer girin.")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+odev2=input("odev2:")
+e=float(odev2)
+
+if e<0 or e>100:
+    print("Lütfen not aralığında sayısal bir deger giriniz.")
+elif type(e) is not float:
+    print("lütfen sayısal bir değer girin.")
+
+final=input("final:")
+f=float(final)
+
+if f<0 or f>100:
+    print("Lütfen not aralığında sayısal bir deger giriniz.")
+elif type(f) is not float:
+    print("lütfen sayısal bir değer girin.")
+ort=(x*0.1+y*0.1+z*0.2+t*0.1+e*0.1+f*0.4)
+if ort>=90:
+  print("notunuz:",ort,"harf notunuz:AA-geçtiniz.")
+elif ort>=80:
+    print("notunuz:", ort, "harf notunuz:BA -geçtiniz")
+elif ort>=70:
+    print("notunuz:", ort, "harf notunuz:BB -geçtiniz")
+elif ort>=60:
+    print("notunuz:", ort, "harf notunuz:CB -geçtiniz")
+elif ort>=50:
+    print("notunuz:", ort, "harf notunuz:CC -geçtiniz")
+elif ort>=40:
+    print("notunuz:", ort, "harf notunuz:DC -geçtiniz")
+elif ort>=35:
+    print("notunuz:", ort, "harf notunuz:DD -geçtiniz")
+elif ort<35:
+    print("notunuz:", ort, "harf notunuz:FF -KALDINIZ")
